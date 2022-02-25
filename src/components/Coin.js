04ef4c6e8c1d085892224c6685f2767c205342ff
Cian Butler <ciandevelops@gmail.com>
2022-02-24 23:13:43 -0500

    somehwat responsive

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,15 +1,33 @@
 import React from "react";
 
-const Coin = ({ image, name, price, priceChange }) => {
+const Coin = ({
+  image,
+  name,
+  price,
+  priceChange,
+  symbol,
+  volume,
+  marketCap,
+}) => {
   return (
     <div className="coin-container p-8">
+      <div className="coin-row flex justify-between text-xl font-black underline">
+        <h1>Name</h1>
+        <h1>Price</h1>
+        <h1 className="hidden md:flex">Volume</h1>
+        <h1>Change</h1>
+        <h1 className="hidden md:flex">Marketcap</h1>
+      </div>
       <div className="coin-row flex justify-between p-4">
         <div className="coin flex">
           <img src={image} alt="crypto logo" className="h-[30px] w-[30px]" />
-          <h1 className="text-xl font-bold ml-2">{name}</h1>
+          <p className="text-xl font-bold ml-2">{name}</p>
+          <p className="coin-symbol hidden md:flex">{symbol}</p>
         </div>
 
         <p className="coin-price">${price.toFixed(2)}</p>
+
+        <p className="coin-volume hidden md:flex">{volume.toLocaleString()}</p>
 
         {priceChange < 0 ? (
           <p className="price-change text-[#f00606]">
@@ -20,6 +38,10 @@ const Coin = ({ image, name, price, priceChange }) => {
             {priceChange.toFixed(2)}%
           </p>
         )}
+
+        <p className="coin-marketcap hidden md:flex">
+          {marketCap.toLocaleString()}
+        </p>
       </div>
     </div>
   );
